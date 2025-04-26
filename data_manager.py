2bb06597d33c7961803072ab3032b36c823b5da8
Darren Brady <dcgbfinancial@gmail.com>
2025-04-26 05:45:07 +0000

    addition of OOP coding

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,25 @@
 # handles the interaction between the Google Sheets
+
+import requests
+from requests.auth import HTTPBasicAuth
+from pprint import pprint
+from dotenv import load_dotenv 
+import os
+
+class Data_Manager:
+
+    def __init__(self):
+        self.username = os.environ()
+        self.password = os.environ() 
+        self.auth = HTTPBasicAuth(self.username, self.password)
+        self.flight_data = {}
+
+    def get_sheet_data(self):
+
+FLIGHT_URL = 'https://api.sheety.co/9a5f2f38c579638928123fbfa4eab617/flightDeals/prices'
+
+
+f_response = requests.get(url=FLIGHT_URL, 
+                          auth= HTTPBasicAuth(USERNAME, PASSWORD)
+)
+pprint(f_response.json())
