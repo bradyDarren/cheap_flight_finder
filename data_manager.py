df764a040d3cbd841aa523f84169686d306c9693
Darren Brady <dcgbfinancial@gmail.com>
2025-04-26 08:46:31 +0000

    refactoring and adding of modules

--- data_manager.py
+++ data_manager.py
@@ -6,20 +6,27 @@ from pprint import pprint
 from dotenv import load_dotenv 
 import os
 
+# Loading all of the environmental variables within our .env file.
+load_dotenv() 
+
+FLIGHT_URL = 'https://api.sheety.co/9a5f2f38c579638928123fbfa4eab617/flightDeals/prices'
+
 class Data_Manager:
 
+    # inititalization function
     def __init__(self):
-        self.username = os.environ()
-        self.password = os.environ() 
+        self.username = os.environ.get('USERNAME')
+        self.password = os.environ.get('PASSWORD') 
         self.auth = HTTPBasicAuth(self.username, self.password)
         self.flight_data = {}
 
     def get_sheet_data(self):
 
-FLIGHT_URL = 'https://api.sheety.co/9a5f2f38c579638928123fbfa4eab617/flightDeals/prices'
+        f_response = requests.get(url=FLIGHT_URL, 
+                                auth= self.auth
+        )
+        data = f_response.json()
+        self.flight_data = data['prices']
+        # pprint(sheet_data)
+        return self.flight_data
 
-
-f_response = requests.get(url=FLIGHT_URL, 
-                          auth= HTTPBasicAuth(USERNAME, PASSWORD)
-)
-pprint(f_response.json())
