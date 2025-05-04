3b9757438cd03da701d0be21bcf0e0ac2867b406
Darren Brady <dcgbfinancial@gmail.com>
2025-05-04 01:17:31 +0000

    completed testing of changing the value of the our iataCode within our Google sheet

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from data_manager import Data_Manager
+from pprint import pprint
+from flight_search import FlightSearch
+import time
 
 test = Data_Manager()
-print(test.get_sheet_data())
+sheet = test.get_sheet_data()
+
+# pprint(sheet)
+
+search = FlightSearch()
+
+for row in sheet: 
+    if row['iataCode'] == '':
+        row['iataCode'] = search.get_iatacode(row['city'])
+        time.sleep(2)
+
+test.flight_data = sheet
+test.change_iataCode()
