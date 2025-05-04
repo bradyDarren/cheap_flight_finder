d937bd7b04c20bb2b2d9d51ad571d749b67d3b6b
Darren Brady <dcgbfinancial@gmail.com>
2025-05-04 08:32:38 +0000

    initial commit

--- data_manager.py
+++ data_manager.py
@@ -2,7 +2,6 @@
 
 import requests
 from requests.auth import HTTPBasicAuth
-from pprint import pprint
 from dotenv import load_dotenv 
 import os
 
@@ -13,7 +12,7 @@ FLIGHT_URL = 'https://api.sheety.co/9a5f2f38c579638928123fbfa4eab617/flightDeals
 
 class Data_Manager:
 
-    # inititalization function
+    # inititalization function, which obtains the important info from .env file.
     def __init__(self):
         self.username = os.environ.get('USERNAME')
         self.password = os.environ.get('PASSWORD') 
@@ -21,6 +20,7 @@ class Data_Manager:
         # self.token = os.environ.get('BEARER_TOKEN') # if we use the BEARER method of auth.
         self.flight_data = {}
 
+    # gets the sheet data from out google sheets file
     def get_sheet_data(self):
 
         # if we use the BEARER method of auth.
@@ -31,8 +31,18 @@ class Data_Manager:
         f_response = requests.get(url=FLIGHT_URL, 
                                 auth= self.auth,
         )
-        data = f_response.json()
-        self.flight_data = data['prices']
-        # pprint(sheet_data)
+        sheet_data = f_response.json()
+        self.flight_data = sheet_data['prices']
         return self.flight_data
-
+    
+    def change_iataCode(self):
+        for row in self.flight_data:
+          update = {
+              'price':{
+                  'iataCode': row['iataCode']
+              }
+          }
+          response = requests.put(url=f'{FLIGHT_URL}/{row['id']}', 
+                                  json=update,
+                                  auth=self.auth)
+        #   print(response.text)
--- flight_data.py
+++ flight_data.py
@@ -1,1 +1,9 @@
 # handles the structuring of the flight data
+
+class FlightData: 
+    
+    def __init__(self, airport_code, flight_info):
+        self.price = 0 
+        self.departure_airport_code = airport_code
+        self.flight_info = flight_info 
+        
